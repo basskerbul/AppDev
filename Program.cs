0e3398c4870f08b7e2d95e135517104d156a85d2
basskerbul <wpusti@yandex.ru>
2023-11-01 19:24:56 +0300

    f

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static List<int> ReturnNonRepeatingList(List<int> list)
+{
+    List<int> result = new List<int>();
+
+    foreach(int item in list)
+    {
+        if (!list.Contains(item))
+            result.Add(item);
+    }
+    return result;
+}
+static List<int> ReturnNonRepeatingList2(List<int> list)
+{
+    return list.Distinct().ToList();
+}
+
+static void SortingRepetitions(ref List<int> list)
+{
+    Dictionary<int, int> ValueCount = new Dictionary<int, int>();
+    foreach(int item in list)
+    {
+        if (ValueCount.ContainsKey(item))
+            ValueCount[item]++;
+        else
+            ValueCount.Add(item, 1);
+    }
+    ValueCount = ValueCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+    list.Clear();
+    foreach (int item in ValueCount.Keys)
+        list.Add(item);
+}
