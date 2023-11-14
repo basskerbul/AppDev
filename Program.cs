107dd3a7dedcd97adbbfddfbc3111b66e526a403
basskerbul <wpusti@yandex.ru>
2023-11-14 22:00:17 +0300

    calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b,
+//введенные из командной строки, и выводящую результат выполнения на экран.
+
+while(true)
+{
+    Calculator.PrintResult();
+}
+
+static class Calculator
+{
+    public static void PrintResult()
+    {
+        Console.WriteLine("Введите выражение");
+        string? user_answer = Console.ReadLine();
+        if(user_answer != null && user_answer != "")
+        {
+            string[] chars = user_answer.Split(" ");
+            try
+            {
+                int a = int.Parse(chars[0]);
+                int b = int.Parse(chars[2]);
+                switch (chars[1])
+                {
+                    case "+":
+                        Console.WriteLine($"{a + b}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{a - b}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{a * b}");
+                        break;
+                    case "/":
+                        if (b == 0) 
+                            Console.WriteLine("Нельзя делить на 0");
+                        else
+                            Console.WriteLine($"{a / b}");
+                        break;
+                    default:
+                        Console.WriteLine("Оператор не распознан");
+                        break;
+                }
+            }
+            catch { throw new Exception("Данные не распознаны"); }
+        } 
+    }
+}
