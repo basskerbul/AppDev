c40a0d55c59c06c0a4ed383847d8be5dcb799bc5
basskerbul <wpusti@yandex.ru>
2023-11-29 21:08:30 +0300

    reflecsion: program a TestClass

---
+++ TestClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDev
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    class DontSaveAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    class CustomNameAttribute : Attribute
+    {
+        public string CustomFieldName { get; }
+        public CustomNameAttribute(string customFieldName) => CustomFieldName = customFieldName;
+    }
+
+    internal class TestClass
+    {
+        [CustomName("CustomFieldName")]
+        public int I { get; set; }
+        [CustomName("CustomFieldName")]
+        public string? S { get; set; }
+        [CustomName("CustomFieldName")]
+        public decimal? D { get; set; }
+        public char[]? C { get; set; }
+
+        public TestClass() { }
+        
+        public TestClass(int i) => I = i;
+
+        public TestClass(int i, string s, decimal d, char[] c) : this(i) 
+        {
+            S = s;
+            D = d;
+            C = c;
+        }
+    }
+}
